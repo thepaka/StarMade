b3d064e3f1dec59264c2b781c3af7254b59e0324
ThePaka <thepakamaster@free.fr>
2013-07-27 22:49:21 +0200

    Add Blocks.get() and functions to convert position and index

--- scripts/minecraft/blocks.py
+++ scripts/minecraft/blocks.py
@@ -22,15 +22,48 @@ class Blocks:
 
     def add(self, pos, id, data=0):
 
-        _x = pos[0] - 8 - self.orig[0]
-        _y = pos[1] - 8 - self.orig[1]
-        _z = pos[2] - 8 - self.orig[2]
+        index = self.pos_to_index( pos )
 
-        index = int(_x + _z * self.width + _y * ( self.length * self.width ))
-
-        #print "Add block : x=%2d y=%2d z=%2d index=%3d\t _x=%2d _y=%2d _z=%2d"\
-        #      "\t id=%3d data=%2d" % ( x, y, z, index, _x, _y, _z, id, data )
+        #print "Add block : x=%2d y=%2d z=%2d index=%3d"\
+        #      "\t id=%3d data=%2d" % ( pos + (index, id, data) )
 
         self.ids[index] = id
         self.datas[index] = data
 
+    def get(self, pos):
+
+        index = self.pos_to_index( pos )
+
+        id = self.ids[index]
+        data = self.datas[index]
+
+        #print "Get block : x=%2d y=%2d z=%2d index=%3d"\
+        #      "\t id=%3d data=%2d" % ( pos + (index, id, data )
+
+        return id, data
+
+    def pos_to_index(self, pos):
+
+        y_size = self.length * self.width
+        z_size = self.width
+
+        _x = pos[0] - 8 - self.orig[0]
+        _y = pos[1] - 8 - self.orig[1]
+        _z = pos[2] - 8 - self.orig[2]
+
+        return int(_x + _z * z_size + _y * y_size )
+
+    def index_to_pos(self, index):
+
+        y_size = self.length * self.width
+        z_size = self.width
+
+        _y = int( index / y_size )
+        _z = int( ( index - _y * y_size ) / z_size )
+        _x = int( ( index - _y * y_size ) -  _z * z_size )
+
+        x = _x + 8 + self.orig[0]
+        y = _y + 8 + self.orig[1]
+        z = _z + 8 + self.orig[2]
+
+        return ( x, y, z )
